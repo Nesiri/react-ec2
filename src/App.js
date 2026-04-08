3fed51b3b66bf5ba1349a25c567b02c38b9dcb08
Nesiri <nesruhussein57@gmail.com>
2026-04-07 18:19:40 -0600

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Hello,Nesru</p>
+       <p>Nesru</p>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const greetingElement = screen.getByText(/Hello,Nesru/i);
+  const greetingElement = screen.getByText(/Nesru/i);
   expect(greetingElement).toBeInTheDocument();
 });
